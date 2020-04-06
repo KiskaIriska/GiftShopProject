472ae7f70772b8f529934f430564af769e70bf0e
KhafizovaEI-PIbd22 <elena150620@gmail.com>
2020-04-06 10:16:07 +0400

    laba 5

---
+++ GiftShop/GiftShopBusinessLogic/ViewModels/ClientViewMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace GiftShopBusinessLogic.ViewModels
+{
+    [DataContract]
+    public class ClientViewMode
+    {
+    }
+}
--- GiftShop/GiftShopBusinessLogic/ViewModels/OrderViewModel.cs
+++ GiftShop/GiftShopBusinessLogic/ViewModels/OrderViewModel.cs
@@ -2,25 +2,50 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace GiftShopBusinessLogic.ViewModels
 {
+    [DataContract]
     public class OrderViewModel
     {
+        [DataMember]
         public int Id { get; set; }
-        public int GiftSetId { get; set; }
-        [DisplayName("Подарочный набор")]
+
+        [DataMember]
+        public int ClientId { get; set; }
+
+        [DataMember]
+        public int ProductId { get; set; }
+
+        [DataMember]
+        [DisplayName("Клиент")]
+        public string ClientFIO { get; set; }
+
+        [DataMember]
+        [DisplayName("Изделие")]
         public string GiftSetName { get; set; }
+
+        [DataMember]
         [DisplayName("Количество")]
         public int Count { get; set; }
+
+        [DataMember]
         [DisplayName("Сумма")]
         public decimal Sum { get; set; }
+
+        [DataMember]
         [DisplayName("Статус")]
         public OrderStatus Status { get; set; }
+
+        [DataMember]
         [DisplayName("Дата создания")]
         public DateTime DateCreate { get; set; }
+
+        [DataMember]
         [DisplayName("Дата выполнения")]
         public DateTime? DateImplement { get; set; }
-    }
+    }
+
 }
